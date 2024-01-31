5dd36ffa2343fdb7e9299a2807e1eae4d2e7c500
johannesmiedema <jmiedema@students.uni-mainz.de>
2024-01-31 21:52:34 +0100

    Update functions

---
+++ R/predict_freezer.R
@@ -0,0 +1,64 @@
+predict.phenotype <- function(data, sex, MR=1){
+
+  # LOAD ML MODELS --------------------------
+  #Load ML Models - MR1 phenotype classification
+  m.MR1.model <- base::readRDS("../ML_models/male_MR1.rds")
+  f.MR1.model <- base::readRDS("../ML_models/female_MR1.rds")
+
+  #Load MR2 Prediction Models including shifter using MR1 freezing data
+  m.MR2pred.shifter.model <- base::readRDS("../ML_models/male_MR2pred_shifter.rds")
+  f.MR2pred.shifter.model <- base::readRDS("../ML_models/female_MR2pred_shifter.rds")
+
+  # 12 time bins during tone was played
+  bins <- 1:12
+  beta <- 0
+  int <- 0
+  freeze <- rowMeans(data)
+  params <- data.frame()
+
+  #Fit regression model for each animal
+  for (i in 1:nrow(freezing_values)){
+    #transpose x and y
+    y <- t(freezing_values[i,])
+    #impute +1 if y is zero to allow log operation
+    if (0 %in% y){y<-y+1}
+    #fit model
+    mod.loglinear <- lm(log(y)~bins)
+    #obtain beta coefficient
+    beta[i] <- mod.loglinear$coefficients[2]
+    #obtain intercepts
+    int[i] <- mod.loglinear$coefficients[1]
+  }
+
+  #Initialize data frame for regression parameters
+  params <- data.frame(freeze=freeze,beta=beta, int=int)
+
+  #Prediction using loaded model
+
+  #Predict MR1 Phenotype of females
+  if (sex == "female" & MR2_prediction == F){
+    message("Predicting female MR1 Phenotypes using MR1 freezing data")
+    phenotypes <- stats::predict(f.MR1.model, newdata = params)
+    #Transform values as the model is a glm model
+    phenotypes <- ifelse(phenotypes>=0.5, "phasic", "sustained")
+    phenotypes <- factor(phenotypes, levels = c("sustained", "phasic"))
+  }
+  #Predict MR2 Phenotypes of females including shifters
+  if (sex == "female" & MR2_prediction == T){
+    message("Predicting female MR2 Phenotypes including shifters using MR1 freezing data")
+    phenotypes <- stats::predict(f.MR2pred.shifter.model, newdata = params)
+  }
+  #Predict MR1 Phenotype of males
+  if (sex == "male" & MR2_prediction == F){
+    message("Predicting male MR1 Phenotypes using MR1 freezing data")
+    phenotypes <- stats::predict(m.MR1.model, newdata = params)
+  }
+  #Predict MR2 Phenotypes of males including shifters
+  if (sex == "male" & MR2_prediction == T ){
+    message("Predicting male MR2 Phenotypes including shifters using MR1 freezing data")
+    phenotypes <- stats::predict(m.MR2pred.shifter.model, newdata = params)
+  }
+  message("Classified  ", length(phenotypes), " animals. ")
+  return(list(phenotypes, params))
+}
+
