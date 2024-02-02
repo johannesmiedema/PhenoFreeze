9d7d9b3c19bbf0ded101698aa3ded93a0230c184
johannesmiedema <jmiedema@students.uni-mainz.de>
2024-02-02 23:41:09 +0100

    add new MR2 Models using MR1 data additionally

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -20,13 +20,24 @@ f_MR2$class <- as.factor(f_MR2$class)
 m_MR1$class <- as.factor(m_MR1$class)
 m_MR2$class <- as.factor(m_MR2$class)
 
-#Train Female Models - logistic regression
+#Prepare MR2 datasets as merge of MR1 and MR2
+f_MR2 <- merge(f_MR1, f_MR2, by = "id")
+f_MR2 <- f_MR2[,c(3:5, 8:11)]
+colnames(f_MR2) <- c("freeze_MR1", "beta_MR1", "int_MR1", "freeze_MR2", "beta_MR2",
+                     "int_MR2", "class")
+
+m_MR2 <- merge(m_MR1, m_MR2, by = "id")
+m_MR2 <- m_MR2[,c(3:5, 8:11)]
+colnames(m_MR2) <- c("freeze_MR1", "beta_MR1", "int_MR1", "freeze_MR2", "beta_MR2",
+                     "int_MR2", "class")
+
+#Train Female Models - logistic regression and randomForest
 female_MR1 <- stats::glm(class ~., data =f_MR1[,3:6], family = "binomial")
-female_MR2 <- stats::glm(class ~., data =f_MR2[,3:6], family = "binomial")
+female_MR2 <- randomForest::randomForest(class~., data =f_MR2)
 
 #Train Male Models - RandomForest and SVM using a linear kernel
 male_MR1 <- randomForest::randomForest(class~., data =m_MR1[,3:6])
-male_MR2 <- e1071::svm(class~., data = m_MR2[,3:6], kernel = "linear")
+male_MR2 <- e1071::svm(class~., data = m_MR2, kernel = "linear")
 
 #Save the models internally to sys-data to be lazy loaded later on
 usethis::use_data(female_MR1, female_MR2, male_MR1, male_MR2, internal = TRUE,
