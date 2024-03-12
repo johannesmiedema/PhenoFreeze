f8ef4e0da1a13a5e387d2c1cf8757100c43a0c7f
johannesmiedema <jmiedema@students.uni-mainz.de>
2024-03-12 19:32:00 +0100

    include shifter prediction 3 class model

--- R/classify_freezer.R
+++ R/classify_freezer.R
@@ -5,12 +5,15 @@
 #' @param data_MR2 dataframe containing freezing values MR2 of bins 13 - 24 as a character
 #' @param sex "female" or "male" specifying the sex
 #' @param MR 1 or 2 specifying the Memory Retrieval Session 1 or 2
+#' @param shifter boolean argument specifying if shifters should be included in prediction
 #' @import randomForest
 #' @import e1071
 #' @export classify_freezer
 #' @export
 
-classify_freezer <- function(data_MR1, data_MR2, sex, MR){
+classify_freezer <- function(data_MR1, data_MR2, sex, MR, shifter){
+
+  message("UPDATE")
 
   #####################################
   #-------PARAMETER VALIDATION--------#
@@ -21,56 +24,52 @@ classify_freezer <- function(data_MR1, data_MR2, sex, MR){
   }
   #Validate data_MR2 for MR2 Classification
   if (MR == 2 & missing(data_MR2)){
-    stop("data_MR1 AND data_MR2 are required for MR2 classification")
+    if (shifter == F){
+      stop("data_MR2 is required for MR2 classification without shifter phenotypes")
+    }
   }
-  #If MR2 Classification is requested, both MR1 and MR2 dataset should be input
+  #If MR2 Shifter Classification is requested, both MR1 and MR2 dataset should be input
   if (MR == 2 & missing(data_MR1)){
-    stop("data_MR1 AND data_MR2 are required for MR2 classification")
-  }
-  #Validate type data_MR1 as data.frame
-  if (!is.data.frame(data_MR1)){
-    stop("data_MR1 has to be an object of type data.frame")
+    if (shifter == T){
+      stop("data_MR1 AND data_MR2 are required for MR2 shifter classification")
+    }
   }
 
-  #Validate length of data_MR1
-  if (ncol(data_MR1) < 12){
-    stop("data_MR1 must consist of at least 12 columns of tone presentation")
+  if (MR == 1){
+    #Validate type data_MR1 as data.frame
+    if (!is.data.frame(data_MR1)){
+      stop("data_MR1 has to be an object of type data.frame")
+    }
+
+    #Validate length of data_MR1
+    if (ncol(data_MR1) != 12){
+      stop("data_MR1 must consist of 12 columns of tone presentation")
+    }
   }
 
-  #Validate column names "13" to "24"
-  if ((all(as.character(c(13:24)) %in% colnames(data_MR1))) == FALSE){
-    stop("data_MR1 must have column names 13 to 24 of type character")
+  if (MR == 2 & shifter == T){
+    #Validate type data_MR1 as data.frame
+    if (!is.data.frame(data_MR1)){
+      stop("data_MR1 has to be an object of type data.frame")
+    }
+
+    #Validate length of data_MR1
+    if (ncol(data_MR1) != 12){
+      stop("data_MR1 must consist of 12 columns of tone presentation")
+    }
   }
 
   #Validate Input data_MR2 if MR = 2 is specified
   if (MR == 2){
-
     #Validate type data_MR2 as data.frame
     if (!is.data.frame(data_MR2)){
       stop("data_MR2 has to be an object of type data.frame")
     }
 
     #Validate length of data_MR2
-    if (ncol(data_MR2) < 12){
-      stop("data_MR2 must consist of at least 12 columns of tone presentation")
+    if (ncol(data_MR2) != 12){
+      stop("data_MR2 must consist of 12 columns of tone presentation")
     }
-
-    #Validate column names "13" to "24" of data_MR2
-    if ((all(as.character(c(13:24)) %in% colnames(data_MR2))) == FALSE){
-      stop("data_MR1 must have column names 13 to 24 of type character")
-    }
-  }
-
-  #Transform input data_MR1 if it does not already consist of 12 time bins
-  #Extract column names 13 to 24
-  if (ncol(data_MR1) > 12){
-    data_MR1 <- base::subset(data_MR1, select = as.character(c(13:24)))
-  }
-
-  #Transform input data_MR2 if it does not already consist of 12 time bins
-  #Extract column names 13 to 24
-  if(MR == 2){
-    data_MR2 <- base::subset(data_MR2, select = as.character(c(13:24)))
   }
 
   #Validate sex parameter
@@ -83,33 +82,35 @@ classify_freezer <- function(data_MR1, data_MR2, sex, MR){
     stop("MR must be an Integer of 1 or 2")
   }
 
-  #####################################
-  #--------MR1-REGRESSION-------------#
+  if (!(missing(data_MR1))){
+    #####################################
+    #--------MR1-REGRESSION-------------#
 
-  # 12 time bins during tone was played
-  #Initialize variables
-  bins <- 1:12
-  beta_MR1 <- 0
-  int_MR1 <- 0
-  #Calculate average freezing of each animal
-  freeze_MR1 <- rowMeans(data_MR1)
+    # 12 time bins during tone was played
+    #Initialize variables
+    bins <- 1:12
+    beta_MR1 <- 0
+    int_MR1 <- 0
+    #Calculate average freezing of each animal
+    freeze_MR1 <- rowMeans(data_MR1)
 
-  #Fit regression model for each animal
-  for (i in 1:nrow(data_MR1)){
-    #transpose x and y
-    y <- t(data_MR1[i,])
-    #pseudocount if y is zero to allow log operation
-    if (0 %in% y){y<-y+1}
-    #fit loglinear model
-    mod.loglinear <- stats::lm(log(y)~bins)
-    #obtain beta coefficients
-    beta_MR1[i] <- mod.loglinear$coefficients[2]
-    #obtain intercepts
-    int_MR1[i] <- mod.loglinear$coefficients[1]
+    #Fit regression model for each animal
+    for (i in 1:nrow(data_MR1)){
+      #transpose x and y
+      y <- t(data_MR1[i,])
+      #pseudocount if y is zero to allow log operation
+      if (0 %in% y){y<-y+1}
+      #fit loglinear model
+      mod.loglinear <- stats::lm(log(y)~bins)
+      #obtain beta coefficients
+      beta_MR1[i] <- mod.loglinear$coefficients[2]
+      #obtain intercepts
+      int_MR1[i] <- mod.loglinear$coefficients[1]
+    }
+
+    #Initialize data frame for regression parameters
+    params_MR1 <- data.frame(freeze_MR1=freeze_MR1, beta_MR1=beta_MR1, int_MR1=int_MR1)
   }
-
-  #Initialize data frame for regression parameters
-  params_MR1 <- data.frame(freeze_MR1=freeze_MR1,beta_MR1=beta_MR1, int_MR1=int_MR1)
 
   #If MR = 1 specified: Return classified animals - prompt success message on console
   if (MR == 1){
@@ -120,7 +121,7 @@ classify_freezer <- function(data_MR1, data_MR2, sex, MR){
       message("Predicting female MR1 Phenotypes using MR1 freezing data")
       phenotypes <- stats::predict(female_MR1, newdata = params_MR1)
       #Transform values as the model is a glm model
-      phenotypes <- ifelse(phenotypes>=0.5, "phasic", "sustained")
+      phenotypes <- ifelse(phenotypes>=0.5, "sustained", "phasic")
       phenotypes <- factor(phenotypes, levels = c("sustained", "phasic"))
     }
     #Predict MR1 Phenotype of males
@@ -137,9 +138,8 @@ classify_freezer <- function(data_MR1, data_MR2, sex, MR){
     }
   }
 
-  #If MR = 2 specified: Perform Regressionanalysis on MR2 and classify using both
-  #MR1 and MR2 regression parameters as suggested before
-  if (MR == 2){
+  #If MR = 2 specified: Perform regression analysis on MR2
+  if (!(missing(data_MR2))){
     #####################################
     #------------REGRESSION-------------#
 
@@ -167,12 +167,13 @@ classify_freezer <- function(data_MR1, data_MR2, sex, MR){
 
     #Initialize data frame for regression parameters
     params_MR2 <- data.frame(freeze_MR2=freeze_MR2,beta_MR2=beta_MR2, int_MR2=int_MR2)
+  }
 
+  #MR 2 shifter classification
+  if (MR == 2 & shifter == T){
     #Merge MR1 and MR2 regression parameters for MR2 classification
     params_merged <- base::cbind(params_MR1, params_MR2)
 
-    #####################################
-    #-------MR2-CLASSIFICATION----------#
     #Predict MR2 Phenotypes of females
     if (sex == "female"){
       message("Predicting female MR2 Phenotypes including shifters using MR1 and MR2 freezing data")
@@ -182,6 +183,30 @@ classify_freezer <- function(data_MR1, data_MR2, sex, MR){
     if (sex == "male"){
       message("Predicting male MR2 Phenotypes including shifters using MR1 and MR2 freezing data")
       phenotypes <- stats::predict(male_MR2, newdata = params_merged)
+    }
+    if (length(phenotypes) == nrow(data_MR2)){
+      message("MR2 classification successfull")
+      message("Classified  ", length(phenotypes), " animals. ")
+      return(phenotypes)
+    } else {
+      stop("Classification error, recheck your input parameters")
+    }
+  }
+
+  #MR 2 Classification without shifter
+  if (MR == 2 & shifter == F){
+    #Predict MR2 Phenotypes of females
+    if (sex == "female"){
+      message("Predicting female MR2 Phenotypes without shifters using MR2 freezing data")
+      phenotypes <- stats::predict(female_MR2_noshifter, newdata = params_MR2)
+      #Transform values as the model is a glm model
+      phenotypes <- ifelse(phenotypes >= 0.5, "sustained", "phasic")
+      phenotypes <- factor(phenotypes, levels = c("sustained", "phasic"))
+    }
+    #Predict MR2 Phenotypes of males
+    if (sex == "male"){
+      message("Predicting male MR2 Phenotypes without shifters using MR2 freezing data")
+      phenotypes <- stats::predict(male_MR2_noshifter, newdata = params_MR2)
     }
     if (length(phenotypes) == nrow(data_MR2)){
       message("MR2 classification successfull")
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -8,17 +8,34 @@ library(usethis)
 
 #read labelled data set
 f_MR1 <- openxlsx::read.xlsx("../Phenotyping-Project/labelled_tables/females_MR1.xlsx", colNames = T)
-f_MR2 <- openxlsx::read.xlsx("../Phenotyping-Project/labelled_tables/females.MR2.xlsx", colNames = T)
+f_MR2 <- openxlsx::read.xlsx("../Phenotyping-Project/labelled_tables/females_MR2shifter.xlsx", colNames = T)
+f_MR2_noshifter <- openxlsx::read.xlsx("../Phenotyping-Project/labelled_tables/females.MR2.xlsx", colNames = T)
 
 m_MR1 <- openxlsx::read.xlsx("../Phenotyping-Project/labelled_tables/males_MR1.xlsx", colNames = T)
-m_MR2 <- openxlsx::read.xlsx("../Phenotyping-Project/labelled_tables/males.MR2.xlsx", colNames = T)
+m_MR2 <- openxlsx::read.xlsx("../Phenotyping-Project/labelled_tables/males_MR2shifter.xlsx", colNames = T)
+m_MR2_noshifter <- openxlsx::read.xlsx("../Phenotyping-Project/labelled_tables/males.MR2.xlsx", colNames = T)
 
 #Declare class as factor
 f_MR1$class <- as.factor(f_MR1$class)
 f_MR2$class <- as.factor(f_MR2$class)
+f_MR2_noshifter$class <- as.factor(f_MR2_noshifter$class)
 
 m_MR1$class <- as.factor(m_MR1$class)
 m_MR2$class <- as.factor(m_MR2$class)
+m_MR2_noshifter$class <- as.factor(m_MR2_noshifter$class)
+
+#Rename MR1 column names
+colnames(f_MR1) <- c("batch", "id", "freeze_MR1", "beta_MR1",
+                     "int_MR1", "class")
+
+colnames(m_MR1) <- c("batch", "id", "freeze_MR1", "beta_MR1",
+                     "int_MR1", "class")
+
+colnames(f_MR2_noshifter) <- c("batch", "id", "freeze_MR2", "beta_MR2",
+                     "int_MR2", "class")
+
+colnames(m_MR2_noshifter) <- c("batch", "id", "freeze_MR2", "beta_MR2",
+                     "int_MR2", "class")
 
 #Prepare MR2 datasets as merge of MR1 and MR2
 f_MR2 <- merge(f_MR1, f_MR2, by = "id")
@@ -31,14 +48,17 @@ m_MR2 <- m_MR2[,c(3:5, 8:11)]
 colnames(m_MR2) <- c("freeze_MR1", "beta_MR1", "int_MR1", "freeze_MR2", "beta_MR2",
                      "int_MR2", "class")
 
-#Train Female Models - logistic regression and randomForest
+#Train Female Models
 female_MR1 <- stats::glm(class ~., data =f_MR1[,3:6], family = "binomial")
-female_MR2 <- randomForest::randomForest(class~., data =f_MR2)
+female_MR2 <- randomForest::randomForest(class~., data = f_MR2)
+female_MR2_noshifter <- stats::glm(class ~., data =f_MR2_noshifter[,3:6], family = "binomial")
 
-#Train Male Models - RandomForest and SVM using a linear kernel
-male_MR1 <- randomForest::randomForest(class~., data =m_MR1[,3:6])
-male_MR2 <- e1071::svm(class~., data = m_MR2, kernel = "linear")
+#Train Male Models
+male_MR1 <- randomForest::randomForest(class~., data = m_MR1[,3:6])
+male_MR2 <- randomForest::randomForest(class~., data =m_MR2)
+male_MR2_noshifter <- e1071::svm(class~., data = m_MR2_noshifter[,3:6], kernel = "linear")
 
 #Save the models internally to sys-data to be lazy loaded later on
-usethis::use_data(female_MR1, female_MR2, male_MR1, male_MR2, internal = TRUE,
-                  overwrite = TRUE)
+usethis::use_data(female_MR1, female_MR2, female_MR2_noshifter,
+                  male_MR1, male_MR2, male_MR2_noshifter,
+                  internal = TRUE, overwrite = TRUE)
